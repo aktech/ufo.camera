b98ccc1f8a7b4064849f4f80583dde5720c0bee2
Amit Kumar <dtu.amit@gmail.com>
2022-08-05 20:29:39 +0100

    Add twiiter share link

--- src/App.js
+++ src/App.js
@@ -66,26 +66,6 @@ function UFOImage() {
         })
     }, [index, generate]);
 
-    const showMeUfoButton = function () {
-        return (
-            <div>
-                <a href="#_" className="relative inline-block text-lg group my-8" onClick={onClickShowUfo}>
-            <span
-                className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-            <span
-                className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-            <span className="relative font-merriweather-sans">Show me a UFO</span>
-            </span>
-                    <span
-                        className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                        data-rounded="rounded-lg">
-              </span>
-                </a>
-            </div>
-
-        )
-    }
 
     const imageBox = function () {
         return (
@@ -159,24 +139,6 @@ function UFOImage() {
     )
     }
 
-    const shareButton = function () {
-        return (
-            <div>
-                <a href="#_"
-                   className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
-                    <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab"
-                         data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                        <path fill="currentColor"
-                              d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z"></path>
-                    </svg>
-                    <span className="relative">Share on Twitter</span>
-                </a>
-
-
-            </div>
-        )
-    }
-
     const ctaButtons = function () {
         return (
             <div
@@ -199,8 +161,8 @@ function UFOImage() {
 
 
                 <div className="md:text-left lg:text-left xl:left">
-                    <a href="#_"
-                       className="inline-flex items-center justify-center px-5 py-4 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
+                    <a   href={`https://twitter.com/intent/tweet?text=Checkout this UFO 👉 ${encodeURIComponent(window.location.href)}`}
+                         className="inline-flex items-center justify-center px-5 py-4 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
                         <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab"
                              data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                             <path fill="currentColor"
