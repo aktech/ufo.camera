68d7d5c7dfb4e9bc15beb249b5d9cb6c14ca496f
Amit Kumar <dtu.amit@gmail.com>
2022-08-05 18:06:36 +0100

    add footer and share button

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function UFOImage() {
     const [imgSrc, setImgSrc] = useState("");
     const [imgDivClass, setImgDivClass] = useState("invisible");
     const [datetime, setDatetime] = useState("")
+    const [text, setText] = useState("")
     const [city, setCity] = useState("")
     const [country, setCountry] = useState("")
     const [shape, setShape] = useState("")
@@ -53,6 +54,7 @@ function UFOImage() {
                 setCountry(data.country)
                 setShape(data.shape)
                 setImgSrc(data.images)
+                setText(data.text)
                 window.location.hash = `#${data.index}`
                 if (data.date_time) {
                     const event = new Date(data.date_time)
@@ -66,7 +68,8 @@ function UFOImage() {
 
     const showMeUfoButton = function () {
         return (
-            <a href="#_" className="relative inline-block text-lg group my-8" onClick={onClickShowUfo}>
+            <div>
+                <a href="#_" className="relative inline-block text-lg group my-8" onClick={onClickShowUfo}>
             <span
                 className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
             <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
@@ -74,11 +77,13 @@ function UFOImage() {
                 className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
             <span className="relative font-merriweather-sans">Show me a UFO</span>
             </span>
-                <span
-                    className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                    data-rounded="rounded-lg">
+                    <span
+                        className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                        data-rounded="rounded-lg">
               </span>
-            </a>
+                </a>
+            </div>
+
         )
     }
 
@@ -88,6 +93,18 @@ function UFOImage() {
                 <figure>
                     <img className="img object-cover object-scale-down" src={imgSrc} alt="ufo"/>
                 </figure>
+            </div>
+        )
+    }
+
+    const textBox = function () {
+        return (
+            <div className="grid m-2 mt-5 text-center sm:mt-16 sm:grid-cols-1 md:grid-cols-1 xl:mt-4">
+                <div
+                    className="p-4  bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700 ">
+                    <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">From the spectator</h5>
+                    <p className="font-normal text-gray-700 dark:text-gray-400">{text}</p>
+                </div>
             </div>
         )
     }
@@ -120,22 +137,68 @@ function UFOImage() {
     const header = function () {
         return (
             <>
-                <h1 className="font-passion-one text-6xl text-black sm:text-5xl xl:text-8xl my-5">ufo.camera</h1>
-                <h2 className="font-passion-one sm:text-1xl xl:text-2xl max-w-lg mx-auto text-gray-400">
-                    (Random images of UFO from NUFORC)
+                <h1 className="font-passion-one text-7xl text-black sm:text-5xl xl:text-8xl my-5">ufo.camera</h1>
+                <h2 className="font-merriweather-sans sm:text-1xl xl:text-2xl max-w-lg mx-auto text-gray-400">
+                    Random images of UFO from <a href="https://nuforc.org/" className="hover:underline">NUFORC</a>
                 </h2>
             </>
             )
+    }
+
+    const footer = function () {
+        return (
+            <footer className="p-4 mt-5">
+                <hr className="mb-4 sm:mx-auto " />
+                <span className="font-merriweather-sans text-lg sm:text-center">
+                   <p className="text-gray-500"> Made by <a href="https://twitter.com/iaktech" className="hover:underline text-black">Amit</a>
+                   • Source on <a href="https://github.com/iaktech" className="hover:underline text-black">GitHub</a>
+                   </p>
+
+                </span>
+            </footer>
+    )
+    }
+
+    const shareButton = function () {
+        return (
+            <div>
+                <a href="#_"
+                   className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
+                    <svg className="mr-2 -ml-1 w-4 h-4" aria-hidden="true" focusable="false" data-prefix="fab"
+                         data-icon="twitter" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path fill="currentColor"
+                              d="M459.4 151.7c.325 4.548 .325 9.097 .325 13.65 0 138.7-105.6 298.6-298.6 298.6-59.45 0-114.7-17.22-161.1-47.11 8.447 .974 16.57 1.299 25.34 1.299 49.06 0 94.21-16.57 130.3-44.83-46.13-.975-84.79-31.19-98.11-72.77 6.498 .974 12.99 1.624 19.82 1.624 9.421 0 18.84-1.3 27.61-3.573-48.08-9.747-84.14-51.98-84.14-102.1v-1.299c13.97 7.797 30.21 12.67 47.43 13.32-28.26-18.84-46.78-51.01-46.78-87.39 0-19.49 5.197-37.36 14.29-52.95 51.65 63.67 129.3 105.3 216.4 109.8-1.624-7.797-2.599-15.92-2.599-24.04 0-57.83 46.78-104.9 104.9-104.9 30.21 0 57.5 12.67 76.67 33.14 23.72-4.548 46.46-13.32 66.6-25.34-7.798 24.37-24.37 44.83-46.13 57.83 21.12-2.273 41.58-8.122 60.43-16.24-14.29 20.79-32.16 39.31-52.63 54.25z"></path>
+                    </svg>
+                    <span className="relative">Share on Twitter</span>
+                </a>
+
+
+            </div>
+        )
+    }
+
+    const ctaButtons = function () {
+        return (
+
+        <div className="grid m-2 mt-5 text-center sm:mt-16 sm:grid-cols-2 sm:gap-x-12 gap-y-4 md:grid-cols-2 md:gap-2 xl:mt-4">
+            {showMeUfoButton()}
+            {shareButton()}
+        </div>
+        )
     }
 
     return (
       <div className="main">
           <div className="max-w-lg mx-auto text-center xl:max-w-5xl">
               {header()}
+              {/*{ctaButtons()}*/}
               {showMeUfoButton()}
+              {shareButton()}
               {imgSrc ? infoBox() : ""}
               {/*{reportLinkBox()}*/}
               {imgData ? imageBox() : ''}
+              {imgData ? textBox() : ''}
+              {footer()}
           </div>
       </div>
   )
