7bb25a627f88d7cd913593cddccca9fb13ca7299
Amit Kumar <dtu.amit@gmail.com>
2022-08-27 14:58:18 +0100

    Add cypress test to check page loads

---
+++ cypress/e2e/homepage/home.cy.js
@@ -0,0 +1,29 @@
+/// <reference types="cypress" />
+
+// Welcome to Cypress!
+//
+// This spec file contains a variety of sample tests
+// for a todo list app that are designed to demonstrate
+// the power of writing tests in Cypress.
+//
+// To learn more about how Cypress works and
+// what makes it such an awesome testing tool,
+// please read our getting started guide:
+// https://on.cypress.io/introduction-to-cypress
+
+describe('example to-do app', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit(Cypress.env('HOST', 'http://localhost:3000'))
+  })
+
+  it('Check Image container loads', () => {
+    cy.get('.show-me-ufo-button').click()
+    cy.url().should('include', '#')
+    cy.get('.image-container', { timeout: 10000 }).should('be.visible');
+
+  })
+})
--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ function UFOImage() {
                 return
             }
             setImgLoading(true)
-            const { data, error } = await supabase
+            const { data, error } = await supabase  // eslint-disable-line @typescript-eslint/no-unused-vars
                 .rpc('random_ufo', {index_id: index_id})
             return data.length !== 0 ? data[0] : {}
         }
@@ -168,22 +168,20 @@ function UFOImage() {
             <div
                 className="grid mt-6 md:mt-6 lg:mt-6 xl:mt-6 grid-cols-2 gap-4 place-content-center justify-center">
                 <div className="md:text-right lg:text-right xl:text-right">
-                    <a href="#_" className="relative inline-block text-lg group" onClick={onClickShowUfo}>
-            <span
-                className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
-            <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
-            <span
-                className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
-            <span className="relative font-merriweather-sans">Show me a UFO</span>
-            </span>
+                    <a href="#_" className="relative inline-block text-lg group show-me-ufo-button" onClick={onClickShowUfo}>
                         <span
-                            className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
-                            data-rounded="rounded-lg">
-              </span>
+                            className="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                        <span className="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-gray-50"></span>
+                        <span
+                            className="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                        <span className="relative font-merriweather-sans">Show me a UFO</span>
+                        </span>
+                                    <span
+                                        className="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                                        data-rounded="rounded-lg">
+                          </span>
                     </a>
                 </div>
-
-
                 <div className="md:text-left lg:text-left xl:left">
                     <a   href={`https://twitter.com/intent/tweet?text=Checkout this UFO 👉 ${encodeURIComponent(window.location.href)}`}
                          className="inline-flex items-center justify-center px-5 py-4 text-base font-medium text-center text-indigo-100 border border-indigo-500 rounded-lg shadow-sm cursor-pointer hover:text-white bg-gradient-to-br from-purple-500 via-indigo-500 to-indigo-500">
