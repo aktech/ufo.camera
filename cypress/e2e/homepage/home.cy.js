10d606ab9ae4f8e948aa988987ee9262dfeda3a6
Amit Kumar <dtu.amit@gmail.com>
2022-08-27 15:21:50 +0100

    fix env var cypress

--- cypress/e2e/homepage/home.cy.js
+++ cypress/e2e/homepage/home.cy.js
@@ -17,7 +17,7 @@ describe('example to-do app', () => {
     // so we must tell it to visit our website with the `cy.visit()` command.
     // Since we want to visit the same URL at the start of all our tests,
     // we include it in our beforeEach function so that it runs before each test
-    cy.visit(Cypress.env('HOST', 'http://localhost:3000'))
+    cy.visit(Cypress.env('HOST'))
   })
 
   it('Check Image container loads', () => {
